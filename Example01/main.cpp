42c273339d9a17f5dbdf06f1992100a4d2d9ee8c
Lukas Gustafson <l_gustafson@fanshaweonline.ca>
2023-09-13 20:53:51 -0400

    Created configured project with fmod linked.

---
+++ Example01/main.cpp
@@ -0,0 +1,18 @@
+#include <fmod.hpp>
+
+
+
+
+// Main entry point to our program
+// argc is the number of arguments
+// argv is the arguments
+// 
+// Examples of input:
+// Example01.exe (1, "")
+// Example01.exe with some parameters (4, {"Example01", "with", "some", "Parameters"})
+int main(int argc, char** argv)
+{
+
+	// 0 means successful, anything else is typically an "error"
+	return 0;
+}
