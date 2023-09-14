a8d523bed55f4fddff2edf3fa1e483d784b5c380
Lukas Gustafson <l_gustafson@fanshaweonline.ca>
2023-09-13 21:19:08 -0400

    Added audio files, implemented fmod and created an example.

--- Example01/main.cpp
+++ Example01/main.cpp
@@ -1,6 +1,67 @@
 #include <fmod.hpp>
+#include <iostream>	// printf
+#include <conio.h>	// _kbhit, _getch
 
+void PlaySound()
+{
+	FMOD::System* system = nullptr;
+	FMOD::Sound* sound = nullptr;
+	FMOD::Channel* channel = nullptr;
+	FMOD_RESULT result;
+	
+	result = FMOD::System_Create(&system);
+	if (result != FMOD_OK)
+	{
+		printf("Failed to create the FMOD System!\n");
+		return;
+	}
 
+	result = system->init(1, FMOD_INIT_NORMAL, nullptr);
+	if (result != FMOD_OK)
+	{
+		printf("Failed to initialize the system!\n");
+		// TODO: FMOD Cleanup system
+		// 'delete system' or see if there is a FMOD::System_Destroy(..)
+		return;
+	}
+
+	result = system->createSound("soundfile.wav", FMOD_DEFAULT, 0, &sound);
+	if (result != FMOD_OK)
+	{
+		printf("Failed to load the sound file: soundfile.wav\n");
+		// TODO: FMOD Cleanup system
+		// 'delete system' or see if there is a FMOD::System_Destroy(..)
+		return;
+	}
+
+	result = system->playSound(sound, 0, false, &channel);
+	if (result != FMOD_OK)
+	{
+		printf("Failed to play the sound!\n");
+		// TODO: FMOD Cleanup system
+		// 'delete system' or see if there is a FMOD::System_Destroy(..)
+		// 'delete sound' or see if there is a FMOD::Sound_Destroy(..)
+		return;
+	}
+
+	printf("Sound playing, press ESC to quit . . .");
+	while (true)
+	{
+		// Wait here until user presses ESCAPE
+		// std::cin >>  // This is a blocking call
+
+		// We need a nonblocking solution
+		// conio.h	_kbhit() _getch()
+		if (_kbhit())
+		{
+			int key = _getch();
+			if (key == 27/*ESCAPE*/)
+			{
+				break;
+			}
+		}
+	}
+}
 
 
 // Main entry point to our program
@@ -12,6 +73,7 @@
 // Example01.exe with some parameters (4, {"Example01", "with", "some", "Parameters"})
 int main(int argc, char** argv)
 {
+	PlaySound();
 
 	// 0 means successful, anything else is typically an "error"
 	return 0;
